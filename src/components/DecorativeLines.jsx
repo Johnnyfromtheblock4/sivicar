4f32db49dd563d1004d6506ea4ff903540730c99
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2026-04-23 19:30:48 +0200

    [UPD] decorative lines svg

--- src/components/DecorativeLines.jsx
+++ src/components/DecorativeLines.jsx
@@ -3,13 +3,14 @@ import "../styles/decorative-lines.css";
 const DecorativeLines = () => {
   return (
     <div className="decorative-lines" aria-hidden="true">
-      <span className="line line-1"></span>
-      <span className="line line-2"></span>
-      <span className="line line-3"></span>
-
-      <span className="dot dot-1"></span>
-      <span className="dot dot-2"></span>
-      <span className="dot dot-3"></span>
+      <img src="/blue.svg" className="decor blue" alt="" draggable={false} />
+      <img
+        src="/orange.svg"
+        className="decor orange"
+        alt=""
+        draggable={false}
+      />
+      <img src="/white.svg" className="decor white" alt="" draggable={false} />
     </div>
   );
 };
